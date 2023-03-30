a5b6ee16fb0ff60d53d9d87d9b462c983579c1ff
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-03-29 21:32:38 -0300

    commit 12

--- lib/pages/busca.dart
+++ lib/pages/busca.dart
@@ -28,6 +28,7 @@ class _BuscaState extends State<Busca> {
   @override
   Widget build(BuildContext context) {
     final formKey = GlobalKey<FormState>();
+    final ColorScheme colors = Theme.of(context).colorScheme;
 
     return Scaffold(
       appBar: const Header(
@@ -36,53 +37,98 @@ class _BuscaState extends State<Busca> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Form(
-          key: formKey,
-          child: ListView(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 20),
-                child: const CampoTexto(
-                  labelText: "Nome do produto",
-                  keyboardType: TextInputType.text,
-                ),
-              ),
-              Ink(
-                decoration: const ShapeDecoration(
-                  color: Colors.blueAccent,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(8),
+        child: ListView(
+          children: [
+            Form(
+              key: formKey,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: SizedBox(
+                      width: 280,
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 20),
+                        child: const CampoTexto(
+                          labelText: "Nome do produto",
+                          keyboardType: TextInputType.text,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.search,
-                    color: Colors.white,
-                    size: 22,
-                  ),
-                ),
+                  // SizedBox(
+                  //   child: Ink(
+                  //     decoration: const ShapeDecoration(
+                  //       color: Colors.blueAccent,
+                  //       shape: RoundedRectangleBorder(
+                  //         borderRadius: BorderRadius.all(
+                  //           Radius.circular(8),
+                  //         ),
+                  //       ),
+                  //     ),
+                  //     child: IconButton(
+                  //       onPressed: () {
+                  //         if (formKey.currentState!.validate()) {}
+                  //       },
+                  //       icon: const Icon(
+                  //         Icons.search,
+                  //         color: Colors.white,
+                  //         size: 22,
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
+                  IconButton(
+                      icon: const Icon(Icons.search),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {}
+                      },
+                      style: IconButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(8),
+                          ),
+                        ),
+                        foregroundColor: colors.onPrimary,
+                        backgroundColor: colors.primary,
+                        disabledBackgroundColor:
+                            colors.onSurface.withOpacity(0.12),
+                        hoverColor: colors.onPrimary.withOpacity(0.08),
+                        focusColor: colors.onPrimary.withOpacity(0.12),
+                        highlightColor: colors.onPrimary.withOpacity(0.12),
+                      )),
+                  // IconButton(
+                  //   onPressed: () {
+                  //     if (formKey.currentState!.validate()) {}
+                  //   },
+                  //   icon: const Icon(
+                  //     Icons.search,
+                  //     color: Colors.black,
+                  //     size: 22,
+                  //   ),
+                  // )
+                  // Botao(
+                  //   backgroundColor: globalStyleColors["azul"],
+                  //   label: "Buscar",
+                  //   fontColor: Colors.white,
+                  //   fontSize: 20,
+                  //   onPressed: () {
+                  //     if (formKey.currentState!.validate()) {
+                  //       ScaffoldMessenger.of(context).showSnackBar(
+                  //         const SnackBar(
+                  //           content: Text('Processing Data'),
+                  //           showCloseIcon: true,
+                  //         ),
+                  //       );
+                  //     }
+                  //   },
+                  // ),
+                ],
               ),
-              // Botao(
-              //   backgroundColor: globalStyleColors["azul"],
-              //   label: "Buscar",
-              //   fontColor: Colors.white,
-              //   fontSize: 20,
-              //   onPressed: () {
-              //     if (formKey.currentState!.validate()) {
-              //       ScaffoldMessenger.of(context).showSnackBar(
-              //         const SnackBar(
-              //           content: Text('Processing Data'),
-              //           showCloseIcon: true,
-              //         ),
-              //       );
-              //     }
-              //   },
-              // ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
